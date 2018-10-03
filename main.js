8c904970b6dd84257540f5a233294b9141a0d5a4
TM2611 <40968375+TM2611@users.noreply.github.com>
2018-10-03 19:26:11 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,34 @@
-console.log('hello')
+let KeyAelement = document.getElementById("key-A")
+
+let KeySelement =  document.getElementById("key-S")
+
+let KeyDelement = document.getElementById("key-D")
+
+
+let audioAelement = 
+    document.getElementById("audio-A")
+
+let audioSelement = 
+    document.getElementById("audio-S")
+
+let audioDelement = 
+    document.getElementById("audio-D")
+
+
+KeyAelement.onclick = () => {
+    console.log("Clicked")
+    audioAelement.play(audioAelement)
+}
+
+
+KeySelement.onclick = () => {
+    console.log("Clicked")
+    audioSelement.play(audioSelement)
+}
+
+KeyDelement.onclick = () => {
+    console.log("Clicked")
+    audioDelement.play(audioDelement)
+}
+
+
